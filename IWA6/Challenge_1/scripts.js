e18cb4890bdfc738168d97d242affcabdca4ca13
LentsweLetlotloMokotedi <lentsweletlotlo0@gmail.com>
2023-04-04 23:17:51 +0200

    Finalizing my IWA6

---
+++ IWA6/Challenge_1/scripts.js
@@ -0,0 +1,11 @@
+const primaryPhone = 'O748105141'
+const secondaryPhone = '0219131568'
+
+// Only change below this line
+
+const primaryValid = (Number(primaryPhone)) == primaryPhone 
+const secondaryValid = (Number(secondaryPhone)) == secondaryPhone
+
+console.log('Primary phone is valid numerical string:', primaryValid)
+console.log('Secondary phone is valid numerical string:', secondaryValid)
+

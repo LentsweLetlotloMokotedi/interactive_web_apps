1b6a02895931ed3a9ecc8cac834c4f29c5357236
LentsweLetlotloMokotedi <lentsweletlotlo0@gmail.com>
2023-05-03 08:35:36 +0200

    CHECKING IF THERE IS STILL SOMETHING THAT DOES NOT WORK

--- IWA_18/scripts.js
+++ IWA_18/scripts.js
@@ -1,17 +1,23 @@
-/**
- * A handler that fires when a user drags over any element inside a column. In
- * order to determine which column the user is dragging over the entire event
- * bubble path is checked with `event.path` (or `event.composedPath()` for
- * browsers that don't support `event.path`). The bubbling path is looped over
- * until an element with a `data-area` attribute is found. Once found both the
- * active dragging column is set in the `state` object in "data.js" and the HTML
- * is updated to reflect the new column.
- *
- * @param {Event} event
- */
-const handleDragOver = (event) => {
+import { createOrderData,updateDragging } from "./data.js";
+import { createOrderHtml,html,updateDraggingHtml } from "./view.js";
+
+
+  //Dragging event1s 
+  
+  /**
+   * A handler that fires when a user drags over any element inside a column. In
+   * order to determine which column the user is dragging over the entire event1
+   * bubble path is checked with `event1.path` (or `event1.composedPath()` for
+   * browsers that don't support `event1.path`). The bubbling path is looped over
+   * until an element with a `data-area` attribute is found. Once found both the
+   * active dragging column is set in the `state` object in "data.js" and the HTML
+   * is updated to reflect the new column.
+   *
+   * @param {Event} event
+   */
+  const handleDragOver = (event) => {
     event.preventDefault();
-    const path = event.path || (event.composedPath && event.composedPath());
+    const path = event1.path || event.composedPath();
     let column = null;
   
     for (const element of path) {
@@ -27,37 +33,122 @@ const handleDragOver = (event) => {
     updateDraggingHtml({ over: column });
   };
   
-  const handleDragStart = (event) => {};
-  const handleDragEnd = (event) => {};
+  let dragged;
+  
+  const handleDragStart = (event1) => {
+    dragged = event1.target;
+  };
+  const handleDragDrop = (event1) => {
+    event1.target.append(dragged);
+  };
+  
+  const handleDragEnd = (event1) => {
+    const background = event1.target.closest("section");
+    background.style.backgroundColor = "";
+  };
+  
+  
+  for (const htmlArea of Object.values(html.area)) {
+    htmlArea.addEventListener("dragover", handleDragOver);
+    htmlArea.addEventListener("dragstart", handleDragStart);
+    htmlArea.addEventListener("drop", handleDragDrop);
+    htmlArea.addEventListener("dragend", handleDragEnd);
+  }
+  
+
+  
   const handleHelpToggle = (event) => {
-    html.help.modal.classList.toggle('is-active');
+    const overlay = html.help.overlay;
+    overlay.show();
+    if (event.target === html.help.cancel) {
+      overlay.close();
+    }
   };
-  const handleAddToggle = (event) => {
-    html.add.modal.classList.toggle('is-active');
+  
+  const handleAddToggle = (event1) => {
+    html.other.add.focus();
+    const overlay = html.add.overlay;
+    overlay.show();
+    if (event1.target === html.add.cancel) {
+      overlay.close();
+      html.add.form.reset();
+    }
   };
-  const handleAddSubmit = (event) => {};
-  const handleEditToggle = (event) => {};
-  const handleEditSubmit = (event) => {};
-  const handleDelete = (event) => {};
+  const handleAddSubmit = (event1) => {
+    event1.preventDefault();
+    const overlay = html.add.overlay;
+    const formData = new FormData(event1.target);
+    const data = Object.fromEntries(formData);
+    const newData = createOrderData(data);
+    const htmlData = createOrderHtml(newData);
+    const append = document.querySelector('[data-area="ordered"]');
+    event1.target.reset();
+    overlay.close();
+    append.appendChild(htmlData);
+  };
   
-  html.add.cancel.addEventListener('click', handleAddToggle);
-  html.other.add.addEventListener('click', handleAddToggle);
-  html.add.form.addEventListener('submit', handleAddSubmit);
+  const handleEditToggle = (event1) => {
+    const overlay = html.edit.overlay;
+    const cancelBtn = html.edit.cancel;
+    const input = html.edit.title;
+    const select = html.edit.table;
+    const option = html.edit.column;
+    event1.target.dataset.id ? overlay.show() : undefined;
+    const id = event1.target.dataset.id ? event1.target.dataset.id : undefined;
   
-  html.other.grid.addEventListener('click', handleEditToggle);
-  html.edit.cancel.addEventListener('click', handleEditToggle);
-  html.edit.form.addEventListener('submit', handleEditSubmit);
-  html.edit.delete.addEventListener('click', handleDelete);
+    input.value = event1.target.dataset.id
+      ? event1.target.querySelector(".order__title").textContent
+      : undefined;
+    select.value = event1.target.dataset.id
+      ? event1.target.querySelector(".order__value").textContent
+      : undefined;
   
-  html.help.cancel.addEventListener('click', handleHelpToggle);
-  html.other.help.addEventListener('click', handleHelpToggle);
+    let section = document.querySelector(`[data-id="${id}"]`);
+    option.value = section ? section.closest("section").dataset.area : "";
   
-  for (const htmlColumn of Object.values(html.columns)) {
-    htmlColumn.addEventListener('dragstart', handleDragStart);
-    htmlColumn.addEventListener('dragend', handleDragEnd);
-  }
+    if (event1.target === cancelBtn) {
+      overlay.close();
+    }
+    html.edit.delete.id = id;
+  };
   
-  for (const htmlArea of Object.values(html.areas)) {
-    htmlArea.addEventListener('dragover', handleDragOver);
-  }
+  const handleEditSubmit = (event1) => {
+    event1.preventDefault();
+    const idRemove = html.edit.delete.id;
+    const orderDelete = document.querySelector(`[data-id="${idRemove}"]`);
+    orderDelete.remove();
+    const overlay = html.edit.overlay;
+    const formData = new FormData(event1.target);
+    const data = Object.fromEntries(formData);
+    const newData = createOrderData(data);
+    const htmlData = createOrderHtml(newData);
+    const appended = document.querySelector(`[data-area="${newData.column}"]`);
+    appended.appendChild(htmlData);
+    event1.target.reset();
+    overlay.close();
+  };
   
+  const handleDelete = (event1) => {
+    const idToBeDeleted = html.edit.delete.id;
+    const orderToBeDeleted = document.querySelector(
+      `[data-id="${idToBeDeleted}"]`
+    );
+    const overlay = html.edit.overlay;
+    orderToBeDeleted.remove();
+    overlay.close();
+  };
+    
+    html.add.cancel.addEventListener("click", handleAddToggle); //
+    html.other.add.addEventListener("click", handleAddToggle); //
+    html.add.form.addEventListener("submit", handleAddSubmit); //
+    
+    html.other.grid.addEventListener("click", handleEditToggle); //
+    html.edit.cancel.addEventListener("click", handleEditToggle); //
+    html.edit.form.addEventListener("submit", handleEditSubmit); //
+    html.edit.delete.addEventListener("click", handleDelete); //
+    
+    html.help.cancel.addEventListener("click", handleHelpToggle); //
+    html.other.help.addEventListener("click", handleHelpToggle); //
+    
+    
+    
